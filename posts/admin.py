9e62bba7c7547f3793977d805d2c7f7280d82ddf
Augusto Miranda Galego <augusto.galego@predictus.inf.br>
2020-10-12 15:01:02 -0300

    first

---
+++ posts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "timestamp", "last_updated"]
+    list_filter = ["last_updated", "timestamp"]
+    search_fields = ["title", "content"]
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
---
+++ posts/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def post_create(request):
+    return HttpResponse("<h1>Create</h1>")
+
+
+def post_detail(request):
+    context = {
+        "title": "Detail"
+    }
+    return render(request, "posts/index.html", context)
+
+
+def post_list(request):
+    if request.user.is_authenticated:
+        context = {
+            "title": "My User List"
+        }
+    else:
+        context = {
+            "title": "List"
+        }
+
+    return render(request, "posts/index.html", context)
+
+
+def post_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
